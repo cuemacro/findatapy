7a1370a8c096681014f01b8cf95cb3b671468019
saeedamen <saeedamen@hotmail.com>
2016-10-02 19:06:05 +0100

    Updated doc

--- findatapy/market/__init__.py
+++ findatapy/market/__init__.py
@@ -3,6 +3,6 @@ __author__ = 'saeedamen'
 from findatapy.market.datavendor import DataVendor
 from findatapy.market.datavendorbbg import DataVendorBBG
 from findatapy.market.ioengine import IOEngine
-from findatapy.market.market import Market
+from findatapy.market.market import Market, FXVolFactory, FXCrossFactory, FXConv
 from findatapy.market.marketdatagenerator import MarketDataGenerator
 from findatapy.market.marketdatarequest import MarketDataRequest
