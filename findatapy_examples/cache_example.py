37fcd2c3387daa75983424afd6a3abc51f66b143
saeedamen <saeedamen@hotmail.com>
2017-02-11 13:55:34 +0000

    Changed example

--- findatapy_examples/cache_example.py
+++ findatapy_examples/cache_example.py
@@ -8,28 +8,46 @@ See below for example of speed increase 2ms when fetching from cache vs nearly a
 
 There is a much bigger difference when we download large amounts of data (eg. intraday minute data)
 
-2017-02-08 14:08:57,592 - __main__ - INFO - Load data from Yahoo directly
-2017-02-08 14:08:57,778 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
-2017-02-08 14:08:57,781 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
-2017-02-08 14:08:57,782 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
-2017-02-08 14:08:57,782 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
-2017-02-08 14:08:57,785 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
-2017-02-08 14:08:57,788 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
-2017-02-08 14:08:57,788 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
-2017-02-08 14:08:58,099 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
-2017-02-08 14:08:58,100 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
-2017-02-08 14:08:58,253 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
-2017-02-08 14:08:58,267 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
-2017-02-08 14:08:58,315 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
-2017-02-08 14:08:58,374 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
-2017-02-08 14:08:58,377 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
-2017-02-08 14:08:58,380 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
-2017-02-08 14:08:58,382 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
-2017-02-08 14:08:58,554 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
-2017-02-08 14:08:58,626 - findatapy.market.ioengine - INFO - Pushed MarketDataRequest_6eef6648406c333a4035cd5e60d0bf2ecf2606d7 to Redis
-2017-02-08 14:08:58,627 - __main__ - INFO - Loaded data from Yahoo directly, now try reading from Redis in-memory cache
-2017-02-08 14:08:58,628 - findatapy.market.ioengine - INFO - Load Redis cache: MarketDataRequest_6eef6648406c333a4035cd5e60d0bf2ecf2606d7
-2017-02-08 14:08:58,629 - __main__ - INFO - Read from Redis cache.. that was a lot quicker!
+2017-02-11 13:54:53,639 - __main__ - INFO - Load data from Yahoo directly
+2017-02-11 13:54:54,119 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:54,120 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:54,120 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:54,127 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:54,127 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:54,128 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:54,129 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:54,680 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:54,687 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:54,691 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:54,693 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:54,749 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:54,765 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:54,770 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:54,780 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:54,791 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:54,938 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:55,013 - findatapy.market.ioengine - INFO - Pushed MarketDataRequest_443__cache_algo-internet_load_return__category-None__cut-NYC__data_source-yahoo__environment-backtest__expiry_date-None__fields-close__finish_date-2017-02-05 00:00:00__freq-daily__freq_mult-1__futures_curve-None__futures_curve_key-None__gran_freq-None__start_date-2002-01-01 00:00:00__tickers-Apple_Citigroup_Microsoft_Oracle_IBM_Walmart_Amazon_UPS_Exxon__trade_side-trade__vendor_fields-Close__vendor_tickers-aapl_c_msft_orcl_ibm_wmt_amzn_ups_xom to Redis
+2017-02-11 13:54:55,014 - __main__ - INFO - Loaded data from Yahoo directly, now try reading from Redis in-memory cache
+2017-02-11 13:54:55,025 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:55,026 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:55,028 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:55,029 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:55,030 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:55,030 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:55,031 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:55,033 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:55,336 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:55,345 - findatapy.market.datavendorweb - INFO - Request Pandas Web data
+2017-02-11 13:54:55,368 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:55,425 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:55,436 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:55,440 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:55,445 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:55,451 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:55,455 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:55,555 - findatapy.market.datavendorweb - INFO - Completed request from Pandas Web.
+2017-02-11 13:54:55,642 - findatapy.market.ioengine - INFO - Pushed MarketDataRequest_440__cache_algo-cache_algo_return__category-None__cut-NYC__data_source-yahoo__environment-backtest__expiry_date-None__fields-close__finish_date-2017-02-05 00:00:00__freq-daily__freq_mult-1__futures_curve-None__futures_curve_key-None__gran_freq-None__start_date-2002-01-01 00:00:00__tickers-Apple_Citigroup_Microsoft_Oracle_IBM_Walmart_Amazon_UPS_Exxon__trade_side-trade__vendor_fields-Close__vendor_tickers-aapl_c_msft_orcl_ibm_wmt_amzn_ups_xom to Redis
+2017-02-11 13:54:55,643 - __main__ - INFO - Read from Redis cache.. that was a lot quicker!
 
 """
 from findatapy.market import Market, MarketDataRequest, MarketDataGenerator, IOEngine
