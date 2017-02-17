55f3425091595f9dae8204d35ca24aabf5c8f813
saeedamen <saeedamen@hotmail.com>
2017-02-17 13:30:03 +0000

    Added extra comments outlining not to share Pool processes between classes

--- findatapy/util/swimpool.py
+++ findatapy/util/swimpool.py
@@ -16,7 +16,7 @@ from findatapy.util import DataConstants
 
 class SwimPool(object):
     """Creating thread and process pools in a generic way. Allows users to specify the underlying thread or multiprocess library
-    they wish to use.
+    they wish to use. Note you can share Pool objects between processes.
 
     """
 
@@ -28,14 +28,33 @@ class SwimPool(object):
 
         self._multiprocessing_library = multiprocessing_library
 
-    def create_pool(self, thread_technique, thread_no, force_new = False):
+        if multiprocessing_library == 'multiprocess':
+            try:
+                import multiprocess;
+                multiprocess.freeze_support()
+            except:
+                pass
+        elif multiprocessing_library == 'multiprocessing_on_dill':
+            try:
+                import multiprocessing_on_dill;
+                multiprocessing_on_dill.freeze_support()
+            except:
+                pass
+        elif multiprocessing_library == 'multiprocessing':
+            try:
+                import multiprocessing;
+                multiprocessing.freeze_support()
+            except:
+                pass
+
+    def create_pool(self, thread_technique, thread_no, force_new = True):
 
         if not(force_new) and self._pool is not None:
             return self._pool
 
         if thread_technique is "thread":
             from multiprocessing.dummy import Pool
-        elif thread_technique is "multiprocessor":
+        elif thread_technique is "multiprocessing":
             # most of the time is spend waiting for Bloomberg to return, so can use threads rather than multiprocessing
             # must use the multiprocessing_on_dill library otherwise can't pickle objects correctly
             # note: currently not very stable
@@ -43,6 +62,9 @@ class SwimPool(object):
                 from multiprocessing_on_dill import Pool
             elif self._multiprocessing_library == 'multiprocess':
                 from multiprocess import Pool
+            elif self._multiprocessing_library == 'multiprocessing':
+                from multiprocessing import Pool
+
             # from pathos.pools import ProcessPool as Pool
 
         self._pool = Pool(thread_no)
