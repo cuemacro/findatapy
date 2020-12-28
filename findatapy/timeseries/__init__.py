1d789ed64a5b9cbb5486143dacb940b80da1819b
saeedamen <saeedamen@hotmail.com>
2020-12-28 16:33:05 +0000

    Spun out calendar to a new script

--- findatapy/timeseries/__init__.py
+++ findatapy/timeseries/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'saeedamen'
 
 from findatapy.timeseries.timezone import Timezone
-from findatapy.timeseries.filter import Calendar
+from findatapy.timeseries.calendar import Calendar
 from findatapy.timeseries.filter import Filter
 from findatapy.timeseries.calculations import Calculations
 from findatapy.timeseries.dataquality import DataQuality
